e342c94db3c4aa4f527c81108f3c42c3695ac731
呉ドギョン <kuredogyon@kuredogyonui-MacBookea.local>
2023-11-20 16:56:34 +0900

    test

---
+++ Assets/Planning/PlanningMemo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanningMemo : MonoBehaviour
+{
+    /*
+     * 본 파일은 기획 내용이며 코딩 내용과는 무관함
+     * 1인 프로젝트를 만들기전에 유사게임 후보로서 놓고자 함
+     * 1. 나는 남자가되고 싶어.
+     * 2. https://namu.wiki/w/eversion
+     * 분류 : 플렛포머
+     * 세부 분류 : 장애물을 통과하며 최종적으로 클리어를 하면 승리 ( 단 승리는 조건부 )
+     * 에비젼 이라는 게임과 비슷하게 기본룰은 플랫포머와 다를거 없지만 차별화를 두고자 함
+     * 그래픽 : 픽셀아트
+     * 사운드 : 무료 혹은 제작
+     * 그래픽 제작 일부 피그마 사용
+     */
+}
